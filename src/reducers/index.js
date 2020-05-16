6336874e8f5a34bda4f1c1f278027d4f3b71a974
Abhijit Mitra <abhijitmitra.in@gmail.com>
2020-05-16 20:19:39 +0530

    pre commit test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
+import {combineReducers} from 'redux';
 
-import todoPage from './todoPage/'
+import todoPage from './todoPage/';
 
-export default combineReducers({todoPage})
+export default combineReducers({todoPage});
